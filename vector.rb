3b13c04c4b17d8cf49f131ad12b3a28584daf61e
Diego <djpg98@gmail.com>
2020-03-02 17:11:48 -0400

    Listo la primera

--- vector.rb
+++ vector.rb
@@ -32,14 +32,49 @@ class Vector2
     end
 
     def to_s()
-        puts "(#@x,#@y)"
+        "(#@x,#@y)"
     end
 
 end
-
     
+class Complejo < Vector2
 
+    def initialize(r_part, i_part=0)
+        super(r_part, i_part)
+    end
 
-    
+    def *(complejo)
+        Complejo.new(@x * complejo.x - @y * complejo.y, @x * complejo.y + @y * complejo.x)
+    end
 
+    def ~@()
+        Complejo.new(@x, -@y)
+    end
+
+    def /(complejo)
+        begin
+            raise "No se puede dividir entre 0" if complejo == Complejo.new(0)
+            numerador = self * ~complejo
+            denominador = (complejo.x ** 2 + complejo.y ** 2).to_f
+            Complejo.new(numerador.x / denominador, numerador.y / denominador)
+        rescue
+            puts "No se puede dividir entre 0"
+        end
+    end
+
+    def to_s()
+        if x != 0
+            if y > 0
+                "#@x + #{@y}i"
+            elsif y == 0
+                "#@x"
+            else
+                "#@x - #{@y.abs}i"
+            end
+        else
+            "#{@y}i"
+        end
+    end
+
+end
     
