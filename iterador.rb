1d99490dc1f28bfaad7fde6d1018ca7792b2155d
Diego <djpg98@gmail.com>
2020-03-06 10:43:55 -0400

    This is the real iterator

--- iterador.rb
+++ iterador.rb
@@ -6,21 +6,5 @@ def producto_cartesiano(fst, snd)
     end
 end
 
+#Probando
 producto_cartesiano(["a", "b", "c"], [4, 5]) {|x| print x, "\n"}
-
-class Producto_cartesiano
-    def initialize(fst, snd)
-        @left = fst
-        @right = snd
-    end
-
-    def each()
-        @left.each do |x|
-            @right.each do |y|
-                yield([x, y])
-            end
-        end
-    end
-end
-
-Producto_cartesiano.new(["a", "b", "c"], [4, 5]).each {|x| print x, "\n"}
